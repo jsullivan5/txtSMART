7617b7978045f0387bb3fc1a19cb02e77e3112e5
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-23 01:04:22 -0600

    Add working send text functionality to sendController.

--- app/components/SendController/SendController.js
+++ app/components/SendController/SendController.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 
 class SendController extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       sendText: ''
-    }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({sendText: event.target.value})
+    this.setState({sendText: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    const { sendText } = this.state
+
+    fetch(`/api/send/${sendText}`)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           Send Txt:
-          <textarea
-                    onChange={(event) => this.handleChange(event)} />
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type='submit' value='Submit' />
+        <input type="submit" value="Submit" />
       </form>
     );
   }
