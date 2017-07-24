6d35271388b4a80dba92937a26e258eebed37daf
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-23 18:51:38 -0600

    Add Jest and Enzyme  dependencies and message tests.

---
+++ app/components/Message/MessageTestStubs.js
@@ -0,0 +1,20 @@
+export const mockDataRecieve = {
+  from: '15555555555',
+  tone: [{tone_name: 'happy', score: 5}],
+  toneView: false,
+  body: "hello"
+}
+
+export const mockDataSend = {
+  from: '+18178732313',
+  tone: [{tone_name: 'happy', score: 5}],
+  toneView: false,
+  body: "hello"
+}
+
+export const mockDataToneView = {
+  from: '+18178732313',
+  tone: [{tone_name: 'happy', score: 5}],
+  toneView: true,
+  body: "hello"
+}
