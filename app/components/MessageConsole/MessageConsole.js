5b7ef724d697a8dd514cd8d71d7a99c2cb780e89
James Sullivan <jamesfsullivan5@gmail.com>
2017-07-25 22:03:03 -0600

    Add user prompt to message List if no user data received.

--- app/components/MessageConsole/MessageConsole.js
+++ app/components/MessageConsole/MessageConsole.js
@@ -4,6 +4,14 @@ import { Message } from '../Message/Message';
 
 const MessageConsole = ({ messageList, handleToneClick, userNum }) => {
 
+  if (userNum.length === 0) {
+    return (
+      <div className='message-console'>
+        <h2>Input your phone number to see your texting history</h2>
+      </div>
+    )
+  }
+
   const messages = messageList
     .filter(message => message.to === userNum || message.from === userNum)
     .map((message, index) => {
