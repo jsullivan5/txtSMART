1b96a596cf525feda6ddcaa8c02b95a03e22b966
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 15:49:43 -0600

    Remove eslint errors from NewMessage.js

--- app/AppHelpers/NewMessage.js
+++ app/AppHelpers/NewMessage.js
@@ -1,11 +1,11 @@
 class newMessage {
   constructor(data) {
-    this.from = data.From,
-    this.to = data.To,
-    this.body = data.Body,
-    this.tone = [],
-    this.toneView = false
-    this.smsId = data.SmsMessageSid
+    this.from = data.From;
+    this.to = data.To;
+    this.body = data.Body;
+    this.tone = [];
+    this.toneView = false;
+    this.smsId = data.SmsMessageSid;
   }
 }
 
