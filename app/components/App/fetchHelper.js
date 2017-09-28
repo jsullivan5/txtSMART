bf9b7fd6161ae9f6b2c4dde776b6ac099fdbe16f
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 16:23:15 -0600

    Remove eslint errors from fetchHelpers.js

--- app/components/App/fetchHelper.js
+++ app/components/App/fetchHelper.js
@@ -1,28 +1,28 @@
-import 'whatwg-fetch';
-
+/* global fetch */
+/* eslint no-console: 0 */
 
 export const getHistory = (component, localStorage) => {
   fetch('/api/history')
     .then(response => response.json())
-    .then(responseData => {
-      component.setState({ messageList: responseData.reverse(), submittedTexts: localStorage })
+    .then((responseData) => {
+      component.setState({ messageList: responseData.reverse(), submittedTexts: localStorage });
     })
     .catch(err => console.log(err));
-}
+};
 
 export const getTone = (component, messageBody, messageKey, messageData, messages) => {
   fetch(`/api/tone/${messageBody}`, { method: 'POST' })
     .then(response => response.json())
-    .then(responseData => {
+    .then((responseData) => {
       const tones = { tone: responseData.document_tone.tone_categories[0].tones };
 
       component.updateIndivMsg(true, messageKey, messageData, messages, tones);
-    })
-}
+    });
+};
 
 export const deleteMessage = (messageData) => {
   fetch(`/api/delete/${messageData.smsId}`, { method: 'DELETE' })
     .then(response => response.json())
     .then(responseData => console.log(responseData))
     .catch(error => console.log(error));
-}
+};
