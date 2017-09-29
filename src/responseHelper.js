0444da3256af74bf7cb5e44d9831c0a3a5bc8635
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 19:07:10 -0600

    Remove linter errors from responseHelper

--- src/responseHelper.js
+++ src/responseHelper.js
@@ -1,40 +1,32 @@
 function formatArray(array) {
-  return array.sort(function(a, b) {
-    return (b.score * 100) - (a.score * 100)
-    })
-    .map(function(tone) {
-      return (tone.tone_name + " : " + ((tone.score * 100).toFixed(2)) + "%")
-  })
+  return array.sort((a, b) => (b.score * 100) - (a.score * 100))
+    .map(tone => (`${tone.tone_name} : ${(tone.score * 100).toFixed(2)}%`));
 }
 
-
-
 function getIntroText(array) {
- var sorted = array.sort(function(a, b) {
-  return (b.score * 100) - (a.score * 100)
-  })
+  const sorted = array.sort((a, b) => (b.score * 100) - (a.score * 100));
 
-  if(sorted[0].score === 0) {
-    return 'Try a longer message. '
+  if (sorted[0].score === 0) {
+    return 'Try a longer message. ';
   }
 
   switch (sorted[0].tone_name) {
     case 'Joy':
-      return 'You sound mostly joyful.'
+      return 'You sound mostly joyful.';
     case 'Anger':
-      return 'You sound mostly angry.'
+      return 'You sound mostly angry.';
     case 'Sadness':
-      return 'You sound mostly sad.'
+      return 'You sound mostly sad.';
     case 'Fear':
-      return 'You sound mostly afraid.'
+      return 'You sound mostly afraid.';
     case 'Disgust':
-      return 'You sound mostly disgusted.'
+      return 'You sound mostly disgusted.';
     default:
-      return 'You sound: '
+      return 'You sound: ';
   }
 }
 
 module.exports = {
-  formatArray: formatArray,
-  getIntroText: getIntroText
-}
+  formatArray,
+  getIntroText,
+};
