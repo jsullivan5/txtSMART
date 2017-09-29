b25262754983423925e029ad5099373124d6fc4b
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 20:24:28 -0600

    Remove linter errors on MessageTestStubs

--- app/components/Message/MessageTestStubs.js
+++ app/components/Message/MessageTestStubs.js
@@ -1,20 +1,20 @@
 export const mockDataRecieve = {
   from: '15555555555',
-  tone: [{tone_name: 'happy', score: 5}],
+  tone: [{ tone_name: 'happy', score: 5 }],
   toneView: false,
-  body: "hello"
-}
+  body: 'hello',
+};
 
 export const mockDataSend = {
   from: '+18178732313',
-  tone: [{tone_name: 'happy', score: 5}],
+  tone: [{ tone_name: 'happy', score: 5 }],
   toneView: false,
-  body: "hello"
-}
+  body: 'hello',
+};
 
 export const mockDataToneView = {
   from: '+18178732313',
-  tone: [{tone_name: 'happy', score: 5}],
+  tone: [{ tone_name: 'happy', score: 5 }],
   toneView: true,
-  body: "hello"
-}
+  body: 'hello',
+};
