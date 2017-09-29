1830e1fc63188b492cd79b961eb36004fc1f9a75
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 18:10:59 -0600

    Remove linter errors from MessageConsole

--- app/components/MessageConsole/MessageConsole.js
+++ app/components/MessageConsole/MessageConsole.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Message from '../Message/Message';
+/* global location */
 /* eslint react/no-find-dom-node: 0 */
 /* eslint react/no-string-refs: 0 */
+/* eslint no-mixed-operators: 0 */
 
 class MessageConsole extends Component {
   componentDidMount() {
@@ -20,46 +22,50 @@ class MessageConsole extends Component {
     }
   }
 
+  // eslint-disable-next-line no-unused-vars
   componentDidUpdate(prevProps, prevState) {
     const elem = ReactDOM.findDOMNode(this.refs.scroller);
-
-    if (elem && this.props.messageList.length > prevProps.messageList.length || this.props.userNum.length > prevProps.userNum.length) {
+    if (elem && this.props.messageList.length >
+       prevProps.messageList.length || this.props.userNum.length >
+       prevProps.userNum.length) {
       elem.scrollIntoView(true);
     }
   }
 
+
   displayMessages() {
     const filterMessages = message => message.to === this.props.userNum ||
       message.from === this.props.userNum;
+    // eslint-disable-next-line no-unused-vars
     const filterCommunity = message => true;
     const filterCallback = location.pathname === '/messages' ? filterMessages : filterCommunity;
 
     if (this.props.messageList.length) {
       return this.props.messageList.filter(filterCallback)
-        .map((message, index) => (<Message
+        .map(message => (<Message
           messageData={message}
-          key={index}
+          key={Math.random()}
           handleToneClick={this.props.handleToneClick}
           location={location.pathname}
           handleDelete={this.props.handleDelete}
         />));
     }
+    return true;
   }
 
   render() {
-    console.log(this.props.messageList);
     if (this.props.userNum.length === 0 || this.props.messageList.length === 0) {
       return (
         <div className="message-console">
-          <p>We are either loading, or you haven't logged in with your number...</p>
-          <img src="https://media.giphy.com/media/7LeoaJAXokpaM/giphy.gif" id="loader-img" />
+          <p>We are either loading, or you have not logged in with your number...</p>
+          <img src="https://media.giphy.com/media/7LeoaJAXokpaM/giphy.gif" id="loader-img" alt="loader" />
         </div>
       );
     }
 
     return (
       <div id="console-wrapper">
-        <p id="reminder-txt">Don't forget to submit texts by starting them with #submit </p>
+        <p id="reminder-txt">{"Don't forget to submit texts by starting them with #submit"}</p>
         <div className="message-console">
           {this.displayMessages()}
           <div ref="scroller" />
@@ -71,6 +77,13 @@ class MessageConsole extends Component {
 
 /* eslint react/forbid-prop-types: 0 */
 MessageConsole.propTypes = {
-  messageList: PropTypes.array.isRequired,
+  messageList: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.object,
+  ]).isRequired,
+  userNum: PropTypes.string.isRequired,
+  handleToneClick: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
+
 export default MessageConsole;
