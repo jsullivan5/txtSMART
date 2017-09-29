f7e648bd3a69b2e51796e8d3b0b032675f7903d5
James Sullivan <jamesfsullivan5@gmail.com>
2017-09-28 18:31:55 -0600

    Remove linter errors from controllerDelete

--- src/controllerDelete.js
+++ src/controllerDelete.js
@@ -1,24 +1,21 @@
-var request = require('request');
-var credentials = require('../credentials.js');
+const request = require('request');
+const credentials = require('../credentials.js');
 
 function deleteMessage(req, res) {
-  var messageId = req.params.msgId;
-  var url = `https://${credentials.sidLive}:${credentials.liveToken}@api.twilio.com/2010-04-01/Accounts/${credentials.sidLive}/Messages/${messageId}`;
+  const messageId = req.params.msgId;
+  const url = `https://${credentials.sidLive}:${credentials.liveToken}@api.twilio.com/2010-04-01/Accounts/${credentials.sidLive}/Messages/${messageId}`;
 
-  request.delete({ uri: url }, function(error, response) {
+  request.delete({ uri: url }, (error, response) => {
     if (error) {
       console.log('error:', error);
       res.status(500).send(error);
-    }
-    else {
+    } else {
       console.log(JSON.stringify(response, null, 2));
       res.status(200).send(response);
     }
   });
 }
 
-
-
 module.exports = {
-  deleteMessage: deleteMessage
+  deleteMessage,
 };
